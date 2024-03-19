86ec4e3e0a4ff3538391a75d9194c0e698ce3661
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-19 22:22:52 +0530

    test: Add unit tests for AuthService to validate API keys, mocking the API key repository. Test cases cover scenarios for invalid API key format, non-existing API keys, and existing API keys.

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,66 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { ApiKey } from '../database/entities/api-key.entity';
 import { AuthService } from './auth.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  //   mocking the api key repository
+
+  const apikeyRepositoryMock = {
+    findOneBy: jest.mock,
+  };
+
+  const apiKeyMock = {
+    id: 'b2edb9e5-8999-4aca-af65-6deacfd1bb9a',
+  } as ApiKey;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(ApiKey),
+          useValue: apikeyRepositoryMock,
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  //   Testing
+  describe('validateApiKey', () => {
+    it('should return false if api key is in Invalid format', async () => {
+      apikeyRepositoryMock.findOneBy = jest.fn().mockResolvedValue(null);
+      const apiKey = 'invalid-api-key-format';
+      const result = await authService.validateApiKey(apiKey);
+      expect(apikeyRepositoryMock.findOneBy).not.toHaveBeenCalled();
+      expect(result).toBe(false);
+    });
+
+    it('should return false if api key does not exists', async () => {
+      apikeyRepositoryMock.findOneBy = jest.fn().mockResolvedValue(null);
+      const apiKey = 'b2edb9e5-8999-4aca-af65-6deacfd1bb9b';
+      const result = await authService.validateApiKey(apiKey);
+      expect(apikeyRepositoryMock.findOneBy).toHaveBeenCalledWith({
+        id: apiKey,
+      });
+      expect(result).toBe(false);
+    });
+
+    it('should return true if api key exists', async () => {
+      apikeyRepositoryMock.findOneBy = jest.fn().mockResolvedValue(apiKeyMock);
+      const result = await authService.validateApiKey(apiKeyMock.id);
+      expect(apikeyRepositoryMock.findOneBy).toHaveBeenCalledWith({
+        id: apiKeyMock.id,
+      });
+      expect(result).toBe(true);
+    });
   });
 });
