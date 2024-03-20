b6f4fb38f0fc40af47b5eb9f78e5a38609ae2096
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-20 22:23:11 +0530

    test 🛠 : Ensure PdfParserService is properly defined and contains postProcessText method for post-processing extracted text from PDF files.

--- src/parsers/pdf-parser/pdf-parser.service.spec.ts
+++ src/parsers/pdf-parser/pdf-parser.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { PdfParserService } from './pdf-parser.service';
 
 describe('PdfParserService', () => {
@@ -14,5 +15,21 @@ describe('PdfParserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('postProcessText', () => {
+    it('Should trim the lines and remove excess inner whitespace to keep a maximum of 3', () => {
+      const input = '      a       b                c            d         ';
+      const expected = 'a  b  c  d';
+      const actual = service['postProcessText'](input);
+      expect(actual).toEqual(expected);
+    });
+
+    it('should keep only one empty line if multiple lines are empty', () => {
+      const input = 'a\n\n\nb\n\n\n\nc\nd';
+      const expected = 'a\n\nb\n\nc\nd';
+      const actual = service['postProcessText'](input);
+      expect(actual).toEqual(expected);
+    });
   });
 });
