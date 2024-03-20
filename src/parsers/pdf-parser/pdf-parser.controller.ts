ce0ab2e2bcd3e17ccccaae9bb0c99e0b3bb07c5f
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-20 15:53:19 +0530

    feat: Implement PdfParserController with Swagger annotations for API security, file upload parsing, and PDF file processing.

--- src/parsers/pdf-parser/pdf-parser.controller.ts
+++ src/parsers/pdf-parser/pdf-parser.controller.ts
@@ -1,12 +1,45 @@
-import { ApiSecurity, ApiTags } from '@nestjs/swagger';
-import { Controller, Get } from '@nestjs/common';
+import { ApiBody, ApiConsumes, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  ParseFilePipeBuilder,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+const uploadSchema = {
+  type: 'object',
+  properties: {
+    file: {
+      type: 'string',
+      format: 'binary',
+    },
+  },
+};
+
+const pdfPipe = new ParseFilePipeBuilder()
+  .addFileTypeValidator({
+    fileType: 'pdf',
+  })
+  .addMaxSizeValidator({
+    maxSize: 1024 * 1024 * 5, // 5mb
+  })
+  .build({
+    fileIsRequired: true,
+  });
 
 @ApiSecurity('apiKey')
 @ApiTags('parsers')
 @Controller({ path: 'parsers/pdf', version: '1' })
 export class PdfParserController {
-  @Get()
-  getHello(): string {
-    return 'Hello World from PdfParserController!';
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: uploadSchema,
+  })
+  @UseInterceptors(FileInterceptor('file'))
+  @Post()
+  parsePdfFromUpload(@UploadedFile(pdfPipe) file: Express.Multer.File) {
+    return { file };
   }
 }
