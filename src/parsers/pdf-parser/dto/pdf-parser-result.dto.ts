df3da777020cebfebada7e981dcfa3d83b45d410
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-21 03:34:54 +0530

    feat(pdf-parser-result): Add PdfParserUploadResultDto and PdfParserUrlResultDto for specifying PDF parsing results with file upload and URL inputs.

--- src/parsers/pdf-parser/dto/pdf-parser-result.dto.ts
+++ src/parsers/pdf-parser/dto/pdf-parser-result.dto.ts
@@ -1,9 +1,35 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, IntersectionType } from '@nestjs/swagger';
+
+import { IsUrl } from 'class-validator';
+
+class UploadResultDto {
+  @ApiProperty({
+    description: 'Original file name of the uploaded file',
+  })
+  originalFileName: string;
+}
 
 export class PdfParserResultDto {
-  @ApiProperty()
-  originalFileName: string;
-
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Parsed and post-processed content of the PDF file',
+  })
   content: string;
 }
+
+class UrlResultDto {
+  @ApiProperty({
+    description: 'Original URL of the PDF file',
+  })
+  @IsUrl()
+  originalUrl: string;
+}
+
+export class PdfParserUploadResultDto extends IntersectionType(
+  PdfParserResultDto,
+  UploadResultDto,
+) {}
+
+export class PdfParserUrlResultDto extends IntersectionType(
+  PdfParserResultDto,
+  UrlResultDto,
+) {}
