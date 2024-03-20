2b6f35b4e506054daf1bc55c2fd49a89a61ced1f
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-21 03:32:27 +0530

    chore(parsers module): Update ParsersModule to include HttpModule for PDF parsing from URLs

--- src/parsers/parsers.module.ts
+++ src/parsers/parsers.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ParsersController } from './parsers.controller';
 import { PdfParserController } from './pdf-parser/pdf-parser.controller';
 import { PdfParserService } from './pdf-parser/pdf-parser.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [ParsersController, PdfParserController],
   providers: [PdfParserService],
 })
