00b44a4d2f4a7fbdb8d04653f28c55939db533e3
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-20 22:26:46 +0530

    testcases(🛠pdf parser controller): Ensure PdfParserController is properly defined and instantiated within the testing environment, along with its associated PdfParserService dependency.

--- src/parsers/pdf-parser/pdf-parser.controller.spec.ts
+++ src/parsers/pdf-parser/pdf-parser.controller.spec.ts
@@ -1,15 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { ConfigModule } from '@nestjs/config';
 import { PdfParserController } from './pdf-parser.controller';
+import { PdfParserService } from './pdf-parser.service';
 
 describe('PdfParserController', () => {
   let controller: PdfParserController;
+  let service: PdfParserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PdfParserController],
+      providers: [PdfParserService],
+      imports: [ConfigModule.forRoot()],
     }).compile();
 
     controller = module.get<PdfParserController>(PdfParserController);
+    service = module.get<PdfParserService>(PdfParserService);
   });
 
   it('should be defined', () => {
