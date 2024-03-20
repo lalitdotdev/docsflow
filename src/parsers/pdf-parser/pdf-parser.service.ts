0a510423db995add05bc3116f40555cb1fae47d1
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-20 22:25:17 +0530

    refactor(pdfparser service ♻): Update PdfParserService to use ConfigService for retrieving POPPLER_BIN_PATH, improving maintainability and configuration handling. Also refactor postProcessText method for readability and efficiency.

--- src/parsers/pdf-parser/pdf-parser.service.ts
+++ src/parsers/pdf-parser/pdf-parser.service.ts
@@ -1,10 +1,12 @@
+import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
 import Poppler from 'node-poppler';
 
 @Injectable()
 export class PdfParserService {
+  constructor(private configService: ConfigService) {}
   async parsePdf(file: Buffer) {
-    const poppler = new Poppler(process.env.POPPLER_BIN_PATH);
+    const poppler = new Poppler(this.configService.get('POPPLER_BIN_PATH'));
 
     let text = await poppler.pdfToText(file, null, {
       maintainLayout: true,
@@ -16,24 +18,16 @@ export class PdfParserService {
     return text;
   }
   private postProcessText(text: string) {
-    // trim each line
-    const lines = text.split('\n').map((line) => line.trim());
+    const processedText = text
+      .split('\n')
+      //trim each line
+      .map((line) => line.trim())
+      //keep only one line if multiple lines are empty
+      .filter((line, index, arr) => line !== '' || arr[index - 1] !== '')
+      //remove whitespace in lines if there are more than 3 spaces
+      .map((line) => line.replace(/\s{3,}/g, '   '))
+      .join('\n');
 
-    // keep only one line if multiple lines are empty
-    const lines2 = lines.filter((line, index) => {
-      if (line === '') {
-        return lines[index - 1] !== '';
-      }
-      return true;
-    });
-
-    // Remove whitespace in lines if there are more than 3 spaces
-
-    const lines3 = lines2.map((line) => {
-      return line.replace(/\s{3,}/g, ' ');
-    });
-
-    const postProcessText = lines3.join('\n');
-    return postProcessText;
+    return processedText;
   }
 }
