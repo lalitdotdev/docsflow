70e1198cd1744fc6d09edf541effa64254b63f42
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-21 22:35:04 +0530

    Pdf Parser Service Test for loadPdfFromUrl.

--- src/parsers/pdf-parser/pdf-parser.service.spec.ts
+++ src/parsers/pdf-parser/pdf-parser.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 import { PdfParserService } from './pdf-parser.service';
 
 describe('PdfParserService', () => {
@@ -8,6 +10,7 @@ describe('PdfParserService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [PdfParserService],
+      imports: [ConfigModule.forRoot(), HttpModule],
     }).compile();
 
     service = module.get<PdfParserService>(PdfParserService);
@@ -18,9 +21,9 @@ describe('PdfParserService', () => {
   });
 
   describe('postProcessText', () => {
-    it('Should trim the lines and remove excess inner whitespace to keep a maximum of 3', () => {
-      const input = '      a       b                c            d         ';
-      const expected = 'a  b  c  d';
+    it('should trim the lines and remove excess inner whitespace to keep a maximum of 3', () => {
+      const input = '       a            b                  c d         ';
+      const expected = 'a   b   c d';
       const actual = service['postProcessText'](input);
       expect(actual).toEqual(expected);
     });
@@ -31,5 +34,34 @@ describe('PdfParserService', () => {
       const actual = service['postProcessText'](input);
       expect(actual).toEqual(expected);
     });
+  });
+
+  // test for loadPdfFromUrl
+  describe('loadPdfFromUrl()', () => {
+    it('should load the pdf from url and parse it', async () => {
+      const url =
+        'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf';
+      const buffer = await service.loadPdfFromUrl(url);
+
+      const expected = 'Dummy PDF file';
+      const actual = await service.parsePdf(buffer);
+
+      expect(actual).toEqual(expected);
+    });
+
+    // it('should throw an error if the URL does not point to a PDF file', async () => {
+    //   const url =
+    //     'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.txt';
+    //   await expect(service.loadPdfFromUrl(url)).rejects.toThrowError(
+    //     'The file extension is not .pdf',
+    //   );
+    // });
+
+    // it('should throw an error if file does not have the pdf magic number', async () => {
+    //   const url = 'https://www.google.com';
+    //   await expect(service.loadPdfFromUrl(url)).rejects.toThrowError(
+    //     'The given URL does not point to a PDF file',
+    //   );
+    // });
   });
 });
