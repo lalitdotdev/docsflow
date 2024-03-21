06334f72559329d182b57fbc288dbcac613369ee
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-21 22:32:25 +0530

    Update Parser test to include HttpModule for PDF parsing from URLs.

--- src/parsers/pdf-parser/pdf-parser.controller.spec.ts
+++ src/parsers/pdf-parser/pdf-parser.controller.spec.ts
@@ -1,18 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 import { PdfParserController } from './pdf-parser.controller';
 import { PdfParserService } from './pdf-parser.service';
 
 describe('PdfParserController', () => {
   let controller: PdfParserController;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   let service: PdfParserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PdfParserController],
       providers: [PdfParserService],
-      imports: [ConfigModule.forRoot()],
+      imports: [ConfigModule.forRoot(), HttpModule],
     }).compile();
 
     controller = module.get<PdfParserController>(PdfParserController);
