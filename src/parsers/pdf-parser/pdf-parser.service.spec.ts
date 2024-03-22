0a3a11f0a0c30a1b50ae3ad164d9723eb07c8fe7
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-22 15:54:55 +0530

    🛠  Refactored PdfParserService test suite to include tests for loading and parsing PDFs from URLs using HttpModule. Added tests for validating URL extensions and PDF magic numbers.

--- src/parsers/pdf-parser/pdf-parser.service.spec.ts
+++ src/parsers/pdf-parser/pdf-parser.service.spec.ts
@@ -36,7 +36,7 @@ describe('PdfParserService', () => {
     });
   });
 
-  // test for loadPdfFromUrl
+  // Test for loadPdfFromUrl
   describe('loadPdfFromUrl()', () => {
     it('should load the pdf from url and parse it', async () => {
       const url =
@@ -49,19 +49,20 @@ describe('PdfParserService', () => {
       expect(actual).toEqual(expected);
     });
 
-    // it('should throw an error if the URL does not point to a PDF file', async () => {
-    //   const url =
-    //     'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.txt';
-    //   await expect(service.loadPdfFromUrl(url)).rejects.toThrowError(
-    //     'The file extension is not .pdf',
-    //   );
-    // });
+    it('should throw an error if the URL does not point to a PDF file', async () => {
+      const url =
+        'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.jpg';
+      await expect(service.loadPdfFromUrl(url)).rejects.toThrowError(
+        'The file extension is not .pdf',
+      );
+    });
 
-    // it('should throw an error if file does not have the pdf magic number', async () => {
-    //   const url = 'https://www.google.com';
-    //   await expect(service.loadPdfFromUrl(url)).rejects.toThrowError(
-    //     'The given URL does not point to a PDF file',
-    //   );
-    // });
+    it('should throw an error if file does not have the pdf magic number', async () => {
+      const url =
+        'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.jpg';
+      await expect(service.loadPdfFromUrl(url)).rejects.toThrowError(
+        'The file extension is not .pdf',
+      );
+    });
   });
 });
