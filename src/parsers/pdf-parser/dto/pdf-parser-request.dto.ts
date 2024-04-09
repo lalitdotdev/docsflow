7129180603333b600b4363b590b8bfc3b7c278b6
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-09 18:03:37 +0530

    feat(dto): Add PdfParserRequestDto with Swagger API property annotation and class-validator for URL validation.

--- src/parsers/pdf-parser/dto/pdf-parser-request.dto.ts
+++ src/parsers/pdf-parser/dto/pdf-parser-request.dto.ts
@@ -3,7 +3,8 @@ import { IsUrl } from 'class-validator';
 
 export class PdfParserRequestDto {
   @ApiProperty({
-    description: 'URL of the PDF file to parse',
+    description:
+      'URL of the PDF file to be parsed and post-processed to the server',
   })
   @IsUrl()
   url: string;
