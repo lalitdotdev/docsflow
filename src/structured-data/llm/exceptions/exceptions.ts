25ea1eca142eda489ae47e0187d341d866791023
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-19 02:50:49 +0530

    exceptions/error-handling: 🔴 Add custom error classes for better error handling: LLMNotAvailableError, LLMApiKeyMissingError, LLMApiKeyInvalidError, LLMBadRequestReceivedError, PromptTemplateFormatError, RefinePromptsInputVariablesError, and RefineReservedChainValuesError.

---
+++ src/structured-data/llm/exceptions/exceptions.ts
@@ -0,0 +1,42 @@
+export class LLMNotAvailableError extends Error {
+  constructor(model = '') {
+    super(`Model ${model} is not available.`);
+  }
+}
+
+export class LLMApiKeyMissingError extends Error {
+  constructor(model = '') {
+    super(`API key for model ${model} is missing.`);
+  }
+}
+export class LLMApiKeyInvalidError extends Error {
+  constructor(model = '') {
+    super(`API key for model ${model} is invalid.`);
+  }
+}
+
+export class LLMBadRequestReceivedError extends Error {
+  constructor(model = '') {
+    super(
+      `Bad request received for model ${model}, the input may be too long for the context window of the model.`,
+    );
+  }
+}
+export class PromptTemplateFormatError extends Error {
+  constructor() {
+    super(`Prompt template could not be formatted with provided chain values.`);
+  }
+}
+export class RefinePromptsInputVariablesError extends Error {
+  constructor(promptTemplate: string, missingInputVariables: string) {
+    super(
+      `${promptTemplate} is missing mandatory input variable: ${missingInputVariables}.`,
+    );
+  }
+}
+
+export class RefineReservedChainValuesError extends Error {
+  constructor(value: string) {
+    super(`Reserved chain value ${value} cannot be used as an input variable.`);
+  }
+}
