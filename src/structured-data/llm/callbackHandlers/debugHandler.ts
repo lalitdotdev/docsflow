f42bc71933deaad5d6a02b15d23ac75afba57fab
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-19 03:08:47 +0530

    Async function handleChainStart in DebugCallbackHandler to handle chain start events. Updates debug report with chain call count, LLM call count, and details of the started chain.

--- src/structured-data/llm/callbackHandlers/debugHandler.ts
+++ src/structured-data/llm/callbackHandlers/debugHandler.ts
@@ -1,5 +1,8 @@
+import { ChainCall, DebugReport } from '../dto/debug.dto';
+
 import { BaseCallbackHandler } from 'langchain/callbacks';
-import { DebugReport } from '../dto/debug.dto';
+import { ChainValues } from 'langchain/schema';
+import { Serialized } from 'langchain/load/serializable';
 
 export class DebugCallbackHandler extends BaseCallbackHandler {
   name = 'DebugCallbackHandler';
@@ -10,5 +13,32 @@ export class DebugCallbackHandler extends BaseCallbackHandler {
 
   get debugReport() {
     return this._debugReport;
+  }
+
+  async handleChainStart(
+    chain: Serialized,
+    inputs: ChainValues,
+    runId: string,
+  ): Promise<void> {
+    const startedChain: ChainCall = {
+      chainName: chain.id.at(-1),
+      runId,
+      start: {
+        inputs,
+      },
+      end: {
+        outputs: null,
+      },
+      error: {
+        err: null,
+      },
+    };
+
+    this._debugReport = {
+      chainCallCount: ++this._chainCallCount,
+      llmCallCount: this._llmCallCount,
+      chains: [...(this._debugReport?.chains ?? []), startedChain],
+      llms: [...(this._debugReport?.llms ?? [])],
+    };
   }
 }
