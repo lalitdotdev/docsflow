aebd4bad555150c816521463fafccaf926a886b0
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-19 08:38:49 +0530

    feat(llm-service/generateOutput): Retrieves available model, validates prompt template, creates LLMChain instance, calls LLMChain with values, handles debug mode, and throws errors for invalid API key or bad request.

--- src/structured-data/llm/llm.service.ts
+++ src/structured-data/llm/llm.service.ts
@@ -1,17 +1,75 @@
 import {
+  LLMApiKeyInvalidError,
   LLMApiKeyMissingError,
+  LLMBadRequestReceivedError,
   LLMNotAvailableError,
+  PromptTemplateFormatError,
 } from './exceptions/exceptions';
 
 import { BaseLanguageModel } from 'langchain/dist/base_language';
+import { ChainValues } from 'langchain/dist/schema';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
 import { ConfigService } from '@nestjs/config';
+import { DebugCallbackHandler } from './callbackHandlers/debugHandler';
 import { Injectable } from '@nestjs/common';
+import { LLMChain } from 'langchain/chains';
 import { Model } from './types/types';
+import { PromptTemplate } from 'langchain/prompts';
+
+// import { ISOLogger } from '@/logger/isoLogger.service';
 
 @Injectable()
 export class LlmService {
   constructor(private configService: ConfigService) {}
+  async generateOutput(
+    model: Model,
+    promptTemplate: PromptTemplate,
+    chainValues: ChainValues,
+    debug = false,
+  ) {
+    const llm = this.retrieveAvailableModel(model);
+    // this.logger.debug(
+    //   `Using model ${model.name} ${model.apiKey ? 'with' : 'without'} API key`,
+    // );
+
+    // Check if the prompt template matches the input variables
+    try {
+      await promptTemplate.format(chainValues);
+    } catch (e) {
+      //   this.logger.error("Prompt template doesn't match input variables");
+      throw new PromptTemplateFormatError();
+    }
+
+    // Create a new LLMChain instance with the given LLM and prompt template
+    const llmChain = new LLMChain({
+      llm,
+      prompt: promptTemplate,
+    });
+
+    console.log('llmChain', llmChain);
+
+    // Call the LLMChain instance with the given chain values and the given debug handler function
+    try {
+      const handler = new DebugCallbackHandler();
+      const output = await llmChain.call(chainValues, debug ? [handler] : []);
+
+      console.log('simple output', output);
+      //   this.logger.debug(`generateOutput completed successfully`);
+
+      return { output, debugReport: debug ? handler.debugReport : null };
+    } catch (e) {
+      if (e?.response?.status && e?.response?.status === 401) {
+        // this.logger.warn('LLMApiKeyInvalidError thrown');
+        throw new LLMApiKeyInvalidError(model.name);
+      }
+      if (e?.response?.status && e?.response?.status === 400) {
+        // this.logger.warn('LLMBadRequestReceivedError thrown');
+        throw new LLMBadRequestReceivedError(model.name);
+      }
+      //   this.logger.warn('Undefined error thrown');
+      throw e;
+    }
+  }
 
   //   Retrieve the available model based on the given model name
   private retrieveAvailableModel(model: Model): BaseLanguageModel {
