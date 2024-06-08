ff502c69914469621b6b5324dcb72b9b7b93ac94
Lalit sharma <geekymonkcp742k@gmail.com>
2024-06-08 13:15:38 +0530

    🧪 Test Cases(JSON Service) : JSON extraction with schema in JsonService: Added test cases for the 'extractWithSchema' method in the JsonService class to ensure proper JSON extraction from text with a given schema. Tests cover successful JSON extraction and handling of invalid JSON output.

--- src/structured-data/json/json.service.spec.ts
+++ src/structured-data/json/json.service.spec.ts
@@ -33,4 +33,31 @@ describe('JsonService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('extractWithSchema()', () => {
+    const text = 'This is a text';
+
+    const schema = '{"title": "string" , "description": "string"}';
+
+    it('should return a json object', async () => {
+      const { json } = await service.extractWithSchema(model, text, schema);
+
+      expect(json).toBeDefined();
+      expect(json).toHaveProperty('title');
+    }, 30000);
+
+    it('should throw an error if the output is not a valid json', async () => {
+      jest.spyOn(llmService, 'generateOutput').mockResolvedValue({
+        output: {
+          text: '{"title": "string"',
+        },
+        debugReport: null,
+      });
+
+      await expect(
+        service.extractWithSchema(model, text, schema),
+      ).rejects.toThrow(InvalidJsonOutputError);
+      //   expect(logger.warn).toHaveBeenCalled();
+    });
+  });
 });
