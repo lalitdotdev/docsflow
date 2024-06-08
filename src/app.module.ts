0dcc8586e6ce0c94b3b8ce100d471d895b454956
Lalit sharma <geekymonkcp742k@gmail.com>
2024-06-08 14:38:59 +0530

    Add StructuredDataModule to AppModule with necessary configurations.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,44 @@
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { ParsersModule } from './parsers/parsers.module';
+import { StructuredDataModule } from './structured-data/structured-data.module';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ParsersModule } from './parsers/parsers.module';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [configuration],
+    }),
     ThrottlerModule.forRoot([
       {
         ttl: 60,
         limit: 20,
       },
     ]),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.POSTGRES_PORT, 10),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      autoLoadEntities: true,
-      synchronize: process.env.NODE_ENV !== 'production',
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('postgres.host'),
+        port: configService.get('postgres.port'),
+        username: configService.get('postgres.user'),
+        password: configService.get('postgres.password'),
+        database: configService.get('postgres.database'),
+        autoLoadEntities: true,
+        synchronize: configService.get('NODE_ENV') !== 'production',
+      }),
+      inject: [ConfigService],
     }),
+
     AuthModule,
     ParsersModule,
+    StructuredDataModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
